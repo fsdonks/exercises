35150340d3fae949efa25a0063ba84046441ae47
Mike Pavlak <michael.m.pavlak.civ@mail.mil>
2017-06-20 15:15:24 -0400

    updated primes

--- src/exercises/Example.clj
+++ src/exercises/Example.clj
@@ -1,11 +1,19 @@
 (ns Example)
 
-;;Generates primes upto x 
+(defn update-primes [primes num]
+  (if (get primes num)
+    (dissoc (into primes (for [p (get primes num)]
+                           (if (get primes (+ num p))
+                             [(+ num p) (conj (get primes (+ num p)) p)]
+                             [(+ num p) [p]]))) num)
+    (assoc primes (* num num) [num])))
+
 (defn generate-primes [x]
   (loop [primes {} num 2]
     (if (<= x num)
       (sort (reduce into (vals primes)))
       (recur (update-primes primes num) (inc num)))))
+
 
 (defn nth-prime [n] ;; scaled distance between primes up to 10^26 is ~2.3
   (last (take n (generate-primes (int (Math/ceil (* (count (str n)) 2.3 n)))))))
@@ -33,3 +41,27 @@
     factors
     (recur (/ n (rho n)) (map int (conj (vec factors) (rho n))))))
 
+(defn number-to-vector [n] (into [] (seq (str n))))
+(defn vector-to-number [v] (Integer. (reduce str v)))
+(defn rotate-vec [v] (into (vector (last v)) (pop v)))
+(defn rotate-digits [n] (->>
+                         (number-to-vector n)
+                         (rotate-vec)
+                         (vector-to-number)))
+(defn digits [n] (count (str n)))
+(defn rotations [n] (sort (take (digits n) (iterate rotate-digits n))))
+
+(defn circular-primes [x]
+  (let [primes (generate-primes x)]
+    (for [p primes]
+      (map #(contains? primes %) (rotations p)))))
+
+
+
+
+               
+    
+    
+  
+
+  
