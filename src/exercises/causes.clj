baeb30d5853e323f2f4f22b50ef022c6c2d10cc4
Mike Pavlak <michael.m.pavlak.civ@mail.mil>
2017-06-23 15:04:18 -0400

    finished 'causes'

--- src/exercises/causes.clj
+++ src/exercises/causes.clj
@@ -99,52 +99,84 @@
 
 ;; Finds all words in "words" that can be made by substituting values in word s 
 (defn substitutions [s words] ;; String, Set -> Set
-  (for [i (range (count s)) a alpha 
-        :let [cand (replace-string s a i)] 
-        :when (get words cand)]
-    cand))
+  (into #{} (for [i (range (count s)) a alpha 
+                  :let [cand (replace-string s a i)] 
+                  :when (get words cand)]
+              cand)))
 
 ;; Finds all words in "words" that can be made by
 ;; adding/inserting a single value in word s  
 (defn additions [s words]  ;; String, Set -> Set
-  (for [i (range (count s)) a alpha 
-        :let [cand (insert-string s a i)]
-        :when (get words cand)]
-    cand))
+  (into #{} (for [i (range (count s)) a alpha 
+                  :let [cand (insert-string s a i)]
+                  :when (get words cand)]
+              cand)))
 
 ;; Finds all words in "words" that can be made by
 ;; removing a single value in word s
 (defn removals [s words] ;; String, Set -> Set
-  (for [i (range (count s))
-        :let [cand (remove-string s i)]
-        :when (get words cand)]
-    cand))
+  (into #{} (for [i (range (count s))
+                  :let [cand (remove-string s i)]
+                  :when (get words cand)]
+              cand)))
 
 ;; Returns a set of words that are "friends" with the string s given words 
 (defn friends-of-word [s words] ;; String, Set -> Set
-  (->>
-   (into #{} (substitutions s words))
-   (into (additions s words))
-   (into (removals s words))))
+  (clojure.set/union
+   (substitutions s words)
+   (additions s words)
+   (removals s words)))
 
-             
+;; Returns result of calls to friends of words for each string in ss 
+(defn multiple-friends [ss words] ;; Coll, Set -> Set
+  (reduce clojure.set/union
+          (for [s ss]
+            (friends-of-word s words))))
 
-  
+;; Returns multiple-friends of friends of source word s
+(defn grow [s words] ;; String, Set -> Set
+  (let [g (friends-of-word s words)]
+    (if (zero? (count g))
+      g
+      (multiple-friends g words))))
+
+(defn solve [s words]
+  (let [g (multiple-friends (friends-of-word s words) words)]
+    (last (take-while #(identity %)
+                      (iterate (fn [{:keys [net size] :as m}]
+                                 (let [next (multiple-friends net words)]
+                                   (if (= (count next) size)
+                                     nil
+                                     (do 
+                                       (println size)
+                                       {:net next
+                                        :size (count next)}))))
+                               {:net g :count (count g)})))))
+
+
+                           
+    
+
+(defn solve [s words] ;; String, Set -> Set 
+  (into #{} (flatten
+             (for [gr (grow s words)]
+               (friends-of-word gr words)))))
+
           
 
-;; ================================================================
+;; =================================================================
 (comment ;; Brute force method
-(defn build-network [s words used friends]
-  {:friends  (clojure.set/difference (into friends (filter-source s words))
-                                     (conj used s))
-   :used (conj used s)})
+  (defn build-network [s words used friends]
+    {:friends  (clojure.set/difference (into friends (filter-source s words))
+                                       (conj used s))
+     :used (conj used s)})
 
-(defn social-network [s words used friends]
-  (let [net (time (build-network s words used friends))]
-    (if (zero? (count (get net :friends)))
-      (get net :used)
-      (recur (first (get net :friends)) words
-             (get net :used) (get net :friends)))))
-) ;;================================================================
+  (defn social-network [s words used friends]
+    (let [net (time (build-network s words used friends))]
+      (if (zero? (count (get net :friends)))
+        (get net :used)
+        (recur (first (get net :friends)) words
+                                    (get net :used) (get net :friends)))))
+  ) ;;=============================================================
 
 
