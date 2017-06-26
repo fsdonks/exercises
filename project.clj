51ed0aeea0363b7f55f2d116829a114311471a67
joinr <vangelder@tantalus.net>
2017-06-26 18:43:52 -0500

    Add POI deps and excercises.powerpoint ns

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [spork "0.2.0.3-SNAPSHOT"]
-                 [incanter "1.5.6"]])
+                 [incanter "1.5.6"]
+                 [org.apache.poi/poi-scratchpad  "3.9"] ;;older POI dep, works.  Migrate to spork.
+                 ])
---
+++ src/exercises/powerpoint.clj
@@ -0,0 +1,10 @@
+;;A quick namespace for exploring ideas about using
+;;the POI libs for munging PPTX presentations programatically.
+(ns exercises.powerpoint
+  (:import [org.apache.poi.xslf.usermodel
+            XMLSlideShow XSLFSlide] ;any more?
+           [java.io FileInputStream] ;maybe unnecessary, used in demo.
+           ))
+
+;;Port examples here...
+
