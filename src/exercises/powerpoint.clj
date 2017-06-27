f139010d3e65370f2f3f868b582443ff442be821
Mike Pavlak <michael.m.pavlak.civ@mail.mil>
2017-06-27 15:53:57 -0400

    added function to copy slides, still looking for way to only copy theme to new ppt

--- src/exercises/powerpoint.clj
+++ src/exercises/powerpoint.clj
@@ -3,12 +3,15 @@
 
 (ns exercises.powerpoint 
    (:import [org.apache.poi.xslf.usermodel 
-             XMLSlideShow XSLFSlide XSLFPictureData] ;any more?
+             XMLSlideShow XSLFSlide XSLFPictureData
+             XSLFSlideLayout XSLFSlideMaster SlideLayout] ;any more?
+            
             [java.io ;maybe unnecessary, used in demo.
              FileInputStream FileOutputStream File]
             [java.nio.file Files Paths]         
             )) 
-
+;;org.apache.poi.hslf.usermodel.HSLFFill
+ 
 ;;Port examples here...
 (comment
 (defn example-pptx []
@@ -30,14 +33,34 @@
     (Files/readAllBytes path)))
 
 ;; given a slide, adds picture file to ppt 
-(defn add-picture [slide filename]
+(defn add-picture [ppt slide filename]
   (let [data (.addPicture ppt
                           (picture->data filename) XSLFPictureData/PICTURE_TYPE_PNG)]
     (.createPicture slide data)))
-    ;;Returns a java XSLFPictureShape obj, not used for anything
+;;Returns a java XSLFPictureShape obj, not used for anything
 
 ;; Given a ppt, creates new slide and adds picture from file for each file
 (defn add-pictures [ppt filenames]
   (doseq [file filenames]
     (add-picture (.createSlide ppt) file)) ppt) ;; returns ppt
 
+(defn new-ppt []
+  (XMLSlideShow.))
+
+(defn read-ppt [filename]
+  (XMLSlideShow. (FileInputStream. filename)))
+
+(defn format-layout [template-file new-ppt]
+  (let [template (read-ppt template-file) 
+        slide-master (first (.getSlideMasters template))
+        layout (.getLayout slide-master SlideLayout/TITLE_AND_CONTENT )]
+    (.createSlide new-ppt layout)) new-ppt)
+
+;; Copy nth slide from source-file and appends it to current-ppt 
+(defn copy-remote-slide [source-file n current-ppt]
+  (let [template (read-ppt source-file)
+        info (nth (.getSlides template) n)
+        slide (.createSlide current-ppt)]
+    (.importContent slide info)) current-ppt) ;;returns updated ppt
+  
+    
