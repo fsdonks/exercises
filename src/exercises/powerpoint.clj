8520dabe894155b224c87ca0b672a093f05e6d96
tom <tom@that-place>
2017-06-28 19:19:19 -0500

    Refactored using macros and mulitple arity fns.
    Used reflection to eliminate the need to hand-jam
    large maps of java types just to get enums out.
    
    Moved incidental utility stuff to exercises.util
    
    Eliminated a lot of java-ish io stuff, leaning on
    prebuilt clojure libraries and/or spork where possible.
    Goal is to avoid all the gritty interop and possible
    reflection hints.
    
    Fixed numerous type hints that weren't placed usefully.
    
    Consolidated/renamed some functions to be a bit more
    idiomatic, to include using multiple arities to
    more closely match the java api.

--- src/exercises/powerpoint.clj
+++ src/exercises/powerpoint.clj
@@ -1,151 +1,145 @@
 ;;A quick namespace for exploring ideas about using
 ;;the POI libs for munging PPTX presentations programatically.
-
-(ns exercises.powerpoint 
+(ns exercises.powerpoint
+  (:require 
+             [clojure.java [io :as io]]
+             [exercises.util :as util]
+             [spork.util [io :refer [list-files fpath fname fext]]])
    (:import [org.apache.poi.xslf.usermodel 
              XMLSlideShow XSLFSlide XSLFPictureData XSLFTheme
              XSLFSlideLayout XSLFSlideMaster SlideLayout] ;any more?
-            [java.io ;maybe unnecessary, used in demo.
-             FileInputStream FileOutputStream File]
-            [java.nio.file Files Paths]         
-            )) 
+            ;[java.io ;maybe unnecessary, used in demo.
+            ; FileInputStream FileOutputStream File]
+            ))
 
-(defn ^XMLSlideShow new-ppt []
-  (^XMLSlideShow XMLSlideShow.))
+;;TODO: Fix reflection warnings.
+(set! *warn-on-reflection* true)
+;;constants and such.
+;;==================
+;;A map of typename to PictureData enumerated type values (ints),
+;;scrapped programtically via reflection.
+(def picturedata-types
+  (into {} (util/get-enums XSLFPictureData PictureType)))
 
-(defn ^XSLFSlide new-slide [ppt]
-  (^XSLFSlide .createSlide ppt))
+(defn get-picturedata-type
+  "Returns java XSLFPictureData.PictureType (int)  from string type-name"
+  [type-name] ;; String -> Int
+  (if (integer? type-name) type-name
+      (util/get-or-err picturedata-types type-name)))
+
+;note: doing this programatically, ended up catching a
+;;fat finger mistake in the original map:
+;;"MEDIDA_AND_TX"
+(def slidelayout-types (into {} (util/get-constants SlideLayout)))
+
+(defn ^SlideLayout get-slidelayout-type
+  "Returns java SlideLayout obj from type."
+  [type-name]
+  (util/get-or-err slidelayout-types type-name))
+
+;;PPT stuff
+;;=========
+;; Reads ppt from file and returns a ppt object 
+(defn ^XMLSlideShow ->pptx
+  "Creates a new Apache POI pptx object.  If a file is specified, 
+   coerces the file to a PPTX object."
+  ([](XMLSlideShow.))
+  ([filename] (XMLSlideShow. (io/input-stream filename))))
+
+(defn ^XSLFSlide ->slide
+  "Mutably adds a slide to existing pptx presentation, returns the newly added 
+   slide."
+  ([^XMLSlideShow ppt] (.createSlide ppt))
+  ([^XMLSlideShow ppt layout] (.createSlide ppt (int layout))))
 
 ;; Saves ppt obj to file
-(defn ^XMLSlideShow save-ppt [ppt filename]
-  (let [out (^FileOutputStream FileOutputStream. (^File File. filename))]
-    (.write ppt out)) ppt)
+;;Using with-open to manage resources better...
+(defn ^XMLSlideShow save-ppt [^XMLSlideShow ppt filename]
+  (with-open [out (io/output-stream filename)]
+    (.write ppt out))
+  ppt)
 
 ;; reads picture data and returns java byte array
-(defn picture->data [filename] ;; String (filename) -> java byte[]
-  (let [path (^Path .toPath (^File File. filename))]
-    (Files/readAllBytes path)))
+(defn ^bytes picture->data [filename] ;; String (filename) -> java byte[]
+  (util/file->bytes filename))
 
-;; Returns java XSLFPictureData.PictureType (int)  from string type-name
-(defn get-picturedata-type [type-name] ;; String -> Int 
-  (let [m {"BMP" XSLFPictureData/PICTURE_TYPE_BMP 
-           "DIB" XSLFPictureData/PICTURE_TYPE_DIB 
-           "EMF" XSLFPictureData/PICTURE_TYPE_EMF 
-           "EPS" XSLFPictureData/PICTURE_TYPE_EPS 
-           "GIF" XSLFPictureData/PICTURE_TYPE_GIF 
-           "JPEG" XSLFPictureData/PICTURE_TYPE_JPEG
-           "PICT" XSLFPictureData/PICTURE_TYPE_PICT 
-           "PNG" XSLFPictureData/PICTURE_TYPE_PNG 
-           "TIFF" XSLFPictureData/PICTURE_TYPE_TIFF 
-           ;;"WDP" XSLFPictureData/PICTURE_TYPE_WDP - no longer suported
-           "WMF" XSLFPictureData/PICTURE_TYPE_WMF 
-           "WPG" XSLFPictureData/PICTURE_TYPE_WPG}]
-    (get m type-name)))
+;;given a slide, adds picture file to ppt
+(defn ^XSLFSlide add-picture
+  [^XMLSlideShow ppt ^XSLFSlide slide filename &
+   {:keys [format] :or {format "PNG"}}]
+  (let [data (.addPicture ppt (picture->data filename) format)]
+    (doto slide (.createPicture data)))) ;; returns slide
 
-;; given a slide, adds picture file to ppt 
-(defn ^XSLFSlide add-picture [ppt slide filename & type]
-  (let [data (.addPicture ppt
-                          (picture->data filename) XSLFPictureData/PICTURE_TYPE_PNG)]
-    (.createPicture slide data)) slide) ;; returns slide
-
-;; Given a ppt, creates new slide and adds picture from file for each file
-(defn ^XMLSlideShow add-pictures [ppt filenames]
+(defn ^XMLSlideShow add-pictures
+  "Given a ppt, creates new slide and adds picture from file for each file"
+  [ppt filenames]
   (doseq [file filenames]
-    (add-picture ppt (^XSLFSlide .createSlide ppt) file)) ppt) ;; returns ppt
+    (add-picture ppt (create-slide ppt) file))
+  ppt) ;; returns ppt
 
-;; Reads ppt from file and returns a ppt object 
-(defn ^XMLSlideShow read-ppt [filename]
-  (^XMLSlideShow XMLSlideShow. (^FileInputStream FileInputStream. filename)))
-
-;; Returns java SlideLayout obj from type 
-  (defn get-slidelayout-type [type-name]
-  (let [m {"BLANK" SlideLayout/BLANK
-           "CHART" SlideLayout/CHART 
-           "CHART_AND_TEXT" SlideLayout/CHART_AND_TEXT
-           "CLIP_ART_AND_TEXT" SlideLayout/CLIP_ART_AND_TEXT 
-           "CLIP_ART_AND_VERT_TX" SlideLayout/CLIP_ART_AND_VERT_TX  
-           "CUST" SlideLayout/CUST
-           "DGM" SlideLayout/DGM
-           "FOUR_OBJ" SlideLayout/FOUR_OBJ  
-           "MEDIDA_AND_TX" SlideLayout/MEDIA_AND_TX  
-           "OBJ_AND_TWO_OBJ" SlideLayout/OBJ_AND_TWO_OBJ  
-           "OBJ_AND_TX" SlideLayout/OBJ_AND_TX  
-           "OBJ_ONLY" SlideLayout/OBJ_ONLY  
-           "OBJ_OVER_TX"   SlideLayout/OBJ_OVER_TX   
-           "OBJ_TX"  SlideLayout/OBJ_TX
-           "PIC_TX"  SlideLayout/PIC_TX
-           "SECTION_HEADER" SlideLayout/SECTION_HEADER
-           "TBL"  SlideLayout/TBL
-           "TEXT" SlideLayout/TEXT
-           "TEXT_AND_CHART" SlideLayout/TEXT_AND_CHART  
-           "TITLE" SlideLayout/TITLE
-           "TITLE_AND_CONTENT" SlideLayout/TITLE_AND_CONTENT
-           "TITLE_ONLY" SlideLayout/TITLE_ONLY
-           "TWO_COL_TX"  SlideLayout/TWO_COL_TX
-           "TWO_OBJ"  SlideLayout/TWO_OBJ
-           "TWO_OBJ_AND_OBJ"  SlideLayout/TWO_OBJ_AND_OBJ
-           "TWO_OBJ_AND_TX"  SlideLayout/TWO_OBJ_AND_TX
-           "TWO_OBJ_OVER_TX"  SlideLayout/TWO_OBJ_OVER_TX
-           "TWO_TX_TWO_OBJ"  SlideLayout/TWO_TX_TWO_OBJ
-           "TX_AND_CLIP_ART"  SlideLayout/TX_AND_CLIP_ART
-           "TX_AND_MEDIA"  SlideLayout/TX_AND_MEDIA
-           "TX_AND_OBJ"  SlideLayout/TX_AND_OBJ
-           "TX_AND_TWO_OBJ"  SlideLayout/TX_AND_TWO_OBJ
-           "TX_OVER_OBJ"  SlideLayout/TX_OVER_OBJ
-           "VERT_TITLE_AND_TX"  SlideLayout/VERT_TITLE_AND_TX
-           "VERT_TITLE_AND_TX_OVER_CHART" SlideLayout/VERT_TITLE_AND_TX_OVER_CHART 
-           "VERT_TX" SlideLayout/VERT_TX}]
-    (get m type-name)))
-
-;; Creates a new slide on new-ppt with layout from template file
-(defn ^XMLSlideShow format-layout [template-file new-ppt]
-  (let [template (read-ppt template-file) 
+(defn ^XMLSlideShow format-layout
+  "Creates a new slide on new-ppt with layout from template file"
+  [template-file new-ppt]
+  (let [template (->pptx template-file) 
         slide-master (first (.getSlideMasters template))
-        layout (.getLayout slide-master SlideLayout/TITLE_AND_CONTENT )]
-    (^XSLFSlide .createSlide new-ppt layout)) new-ppt)
+        layout (.getLayout slide-master SlideLayout/TITLE_AND_CONTENT)
+        _      (->slide new-ppt layout)]
+    new-ppt))
 
-;; Creates a new slide on new-ppt with layout from template file
-;; where slide type determined by type arg (integer - from SlideLayout vars)
-(defn ^XMLSlideShow format-layout-type [template-file new-ppt type]
-  (let [template (read-ppt template-file)
+(defn ^XMLSlideShow format-layout-type
+  "Creates a new slide on new-ppt with layout from template file
+   where slide type determined by type arg (integer - from SlideLayout vars)"
+  [template-file new-ppt type]
+  (let [template (->pptx template-file)
         slide-master (first (.getSlideMasters template))
-        layout (.getLayout slide-master type)]
-    (^XSLFSlide .createSlide new-ppt layout)) new-ppt)
+        layout (.getLayout slide-master type)
+        _      (->slide new-ppt layout)]
+    new-ppt))
 
 ;; Copy nth slide from source-file and appends it to current-ppt 
 (defn ^XSLFSlide copy-remote-slide [source-file n current-ppt]
-  (let [template (read-ppt source-file)
-        info (nth (^XSLFSlide .getSlides template) n)
-        slide (^XSLFSlide .createSlide current-ppt)]
-    (^XSLFSlide .importContent slide info))) ;;returns new-slide
+  (let [template (->pptx source-file)
+        info (nth (.getSlides template) n)]
+    (doto (->slide current-ppt)
+      (.importContent  ^XSLFSlide info)))) ;;returns ->slide
 
-;; Prints out all layouts that are available in ppt
-(defn print-layouts [ppt] ;; Java ppt obj -> nil (Standard Out) 
+(defn print-layouts
+  "Prints out all layouts that are available in ppt"
+  [ppt] ;; Java ppt obj -> nil (Standard Out) 
   (println "Available slide layouts: ")
   (doseq [m (.getSlideMasters ppt)]
     (doseq [l (.getSlideLayouts m)]
       (println (.getType l))))) 
 
-;; Returns java theme object from slide
-(defn ^XSLFTheme get-theme [slide] ;; Java slide obj -> java theme obj
-  (^XSLFTheme .getTheme slide))
+(defn ^XSLFTheme get-theme
+  "Returns java theme object from slide, or if a file is 
+   specified, from the pptx at the file."
+  [tgt] ;; Java slide obj | String -> java theme obj
+  (let [^XSLFSlide slide (if (string? tgt)  
+                             (->slide (->pptx tgt))
+                             tgt)]
+    (.getTheme slide)))
 
-;; Returns java theme object from slide from [filename].pptx
-(defn ^XSLFTheme get-remote-theme [filename]
-  (let [ppt (read-ppt filename)]
-    (get-theme (new-slide ppt))))
+;;File filtering and other IO stuff...
+;;====================================
 
-;; Returns true if filename is specified as a png file
-(defn png? [filename]
+(defn png?
+  "Returns true if filename is specified as a png file"
+  [filename]
   (re-matches #"[\d\w_-]*\.png" filename))
 
-;; Returns a seq of filenames for png files is the current/given directory
-(defn find-images [& dir] ;; Optional - alternative directory to look in
-  (let [dir  (if dir (first dir) ".")]
-    (filter png? (vec  (^String .list (^File File. dir))))))
+(defn file-names [p]
+  (map fname (list-files p)))
 
-;; Returns a seq of filenames for files of given type in current/given directory
-(defn filter-filetype [type & dir] ;; String (file type), Optional alternative directory
+(defn find-images
+  "Returns a seq of filenames for png files is the current/given directory."
+  [& dir] ;; Optional - alternative directory to look in
+  (let [dir  (if dir (first dir) ".")]
+    (filter png? (file-names dir))))
+
+(defn filter-filetype
+  "Returns a seq of filenames for files of given type in current/given directory"
+  [type & dir] ;; String (file type), Optional alternative directory
   (let [dir  (if dir (first dir) ".")]
     (filter #(re-matches (re-pattern (str "[\\d\\w_-]*." type)) %)
-            (^String .list (^File File. dir)))))
+            (file-names  dir))))
